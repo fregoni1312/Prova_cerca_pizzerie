106de48c6a1d84dcfb2dbc8faa95c2716045e391
fregoni1312 <33417641+fregoni1312@users.noreply.github.com>
2017-12-11 21:40:02 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -7,9 +7,8 @@
 			{
 				var numero=document.getElementById("limite").value;
 				var ok=false;
-				var verifica=/^\d{1,2}$/
 				if(document.getElementById("limite").value!=""&&document.getElementById("citta").value!=""&&document.getElementById("query").value!="")
-					if(numero.match(verifica)&&parseInt(numero)<51)
+					if(parseInt(numero)<51)
 						ok=true;
 				return ok; 
 			}
@@ -40,14 +39,20 @@
 			echo " <input type='submit' value='Aggiorna tabella' class='btn'/>";
 			echo "</form>";
 			//Salvo il link di richiestain una variabile
-		  //$indirizzo="https://api.foursquare.com/v2/venues/search?v=20161016&query=$query&limit=$limite&intent=checkin&client_id=3AGUTWIPEQWCBRFCAHIN104WCY0IAPETGLTQIJUDP0JMIC5W&W&client_secret=NH1JI30DQ4YSF5PBSWMCTGPWLHBR1Z11VHYI5ELMV2MNAXNV&near=$citta";
-		    $indirizzo="https://api.foursquare.com/v2/venues/search?v=20161016&query=$query&limit=$limite&intent=checkin&client_id=4DLLUZVXJEQIL0DFCN3B3YFG4EN4W4DMICUVPSNMRD24XKVU&W&client_secret=ZWWMV4LSNXGTZIRIUWHGE5PQDESQ0AHBACUPXVDPTESUTLRX&near=$citta";
+		  	//$indirizzo="https://api.foursquare.com/v2/venues/search?v=20161016&query=$query&limit=$limite&intent=checkin&client_id=3AGUTWIPEQWCBRFCAHIN104WCY0IAPETGLTQIJUDP0JMIC5W&W&client_secret=NH1JI30DQ4YSF5PBSWMCTGPWLHBR1Z11VHYI5ELMV2MNAXNV&near=$citta";
+		    	$indirizzo="https://api.foursquare.com/v2/venues/search?v=20161016&query=$query&limit=$limite&intent=checkin&client_id=4DLLUZVXJEQIL0DFCN3B3YFG4EN4W4DMICUVPSNMRD24XKVU&W&client_secret=ZWWMV4LSNXGTZIRIUWHGE5PQDESQ0AHBACUPXVDPTESUTLRX&near=$citta";
+			//curl_init inizializza una nuova sessione che gestirò con la funzione curl_setopt
 			$ch = curl_init() or die(curl_error());
-			curl_setopt($ch, CURLOPT_URL,$indirizzo);
+			//CURLOPT_URL setta l'indirizzo
+			curl_setopt($ch, CURLOPT_URL,$indirizzo); 
+			//CURLOPT_RETURNTRANSFER per restituire il trasferimento come una stringa
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			//curl_exec restituisce true in caso di successo, altrimenti false
 			$json=curl_exec($ch) or die(curl_error());
+		        //json_decode converte da json in una variabile PHP
 			$dati = json_decode($json);
 
+			//Creo la tabella dove venrrano visualizzate le informazioni
 			echo('<table id="customers" align="center">
 			  <tr>
 				<th>Nome</th>
